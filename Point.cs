8b3f2b1aefb8510785029c5ab238f7df937c042b
Schamneka <k.shamne@napoleonit.com>
2022-07-27 23:38:51 +0500

    day_4

--- Point.cs
+++ Point.cs
@@ -8,6 +8,13 @@ namespace snakeOOP
         public int y;
         public char sym;
 
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw()
         {
          Console.SetCursorPosition( x, y );
