1120dbeb7c17ba9d0780a9852d47717f997038ae
Schamneka <k.shamne@napoleonit.com>
2022-09-06 02:35:53 +0500

    day_11

--- Program.cs
+++ Program.cs
@@ -8,29 +8,29 @@ namespace snakeOOP
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main( string[] args )
+		  {
+			    Console.SetBufferSize( 500, 500 );
 
-          HorizontalLine upLine = new HorizontalLine( 0, 78, 0, '+');
-          HorizontalLine downLine = new HorizontalLine( 0, 78, 24, '+');
-          VerticalLine leftLine = new VerticalLine( 0, 24, 0, '+');
-          VerticalLine rightLine = new VerticalLine( 0, 24, 78, '+');
-          upLine.Draw();
-          downLine.Draw();
-          leftLine.Draw();
-          rightLine.Draw();
+		   	  Walls walls = new Walls( 500, 500 );
+		   	  walls.Draw();
 
+			    // Отрисовка точек	
           Point p = new Point(4, 5, '*');
           Snake snake = new Snake( p, 4, Direction.RIGHT ); 
           snake.Draw();
 
-          FoodCreator foodCreator = new FoodCreator( 80, 25, '$' );
+          FoodCreator foodCreator = new FoodCreator( 100, 50, '$' );
 			    Point food = foodCreator.CreateFood();
 		    	food.Draw();
 
           while(true)
           {
-				        if(snake.Eat( food ) )
+				        if ( walls.IsHit(snake) || snake.IsHitTail() )
+				        {
+					        break;
+				        }
+                if(snake.Eat( food ) )
 				        {
 					         food = foodCreator.CreateFood();
 					        food.Draw();
@@ -47,6 +47,25 @@ namespace snakeOOP
 				         	snake.HandleKey( key.Key );
 				        }
           }
+          WriteGameOver();
+			    Console.ReadLine();
         }
+        static void WriteGameOver()
+		    {
+			    int xOffset = 25;
+			    int yOffset = 8;
+			    Console.ForegroundColor = ConsoleColor.Red;
+			    Console.SetCursorPosition( xOffset, yOffset++ );
+			    WriteText( "============================", xOffset, yOffset++ );
+			    WriteText( "И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++ );
+			    yOffset++;
+			    WriteText( "Автор: Шамне Кирилл", xOffset + 2, yOffset++ );
+			    WriteText( "============================", xOffset, yOffset++ );
+		    }
+        static void WriteText( String text, int xOffset, int yOffset )
+		    {
+			    Console.SetCursorPosition( xOffset, yOffset );
+			    Console.WriteLine( text );
+        }  
     }
 }
--- Snake.cs
+++ Snake.cs
@@ -38,6 +38,17 @@ namespace snakeOOP
          return nextPoint;
       }
 
+      public bool IsHitTail()
+		  {
+			  var head = pList.Last();
+			  for(int i = 0; i < pList.Count - 2; i++ )
+			  {
+				  if ( head.IsHit( pList[ i ] ) )
+					return true;
+			  }
+			  return false;
+		  }
+
       public void HandleKey(ConsoleKey key)
       {  
         if ( key == ConsoleKey.LeftArrow )
